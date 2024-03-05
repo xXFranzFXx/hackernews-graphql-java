8fc2e7d41c33f04be42f611bcf9e452a58099ad3
xXFranzFXx <franz.fernando@testpro.io>
2024-03-05 12:56:06 -0800

    updates schema
    updates schema types and resolvers

--- src/main/java/com/howtographql/hackernews/GraphQLEndpoint.java
+++ src/main/java/com/howtographql/hackernews/GraphQLEndpoint.java
@@ -18,7 +18,7 @@ public class GraphQLEndpoint extends SimpleGraphQLServlet {
         LinkRepository linkRepository = new LinkRepository();
         return SchemaParser.newParser()
                 .file("schema.graphqls")
-                .resolvers(new Query(linkRepository))
+                .resolvers(new Query(linkRepository), new Mutation(linkRepository))
                 .build()
                 .makeExecutableSchema();
     }
--- src/main/java/com/howtographql/hackernews/Link.java
+++ src/main/java/com/howtographql/hackernews/Link.java
@@ -1,13 +1,20 @@
 package com.howtographql.hackernews;
 
 public class Link {
-
+    private final String id;
     private final String url;
     private final String description;
 
     public Link(String url, String description) {
+        this(null, url, description);
+    }
+    public Link(String id, String url, String description) {
+        this.id = id;
         this.url = url;
         this.description = description;
+    }
+    public String getId() {
+        return id;
     }
 
     public String getUrl() {
