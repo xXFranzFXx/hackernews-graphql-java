687476a3d08ee2f8dae10e238d10ec8b473d4c16
xXFranzFXx <franz.fernando@testpro.io>
2024-03-06 15:00:39 -0800

    adds mongodb connector

--- src/main/java/com/howtographql/hackernews/GraphQLEndpoint.java
+++ src/main/java/com/howtographql/hackernews/GraphQLEndpoint.java
@@ -1,8 +1,9 @@
 package com.howtographql.hackernews;
-
 import com.coxautodev.graphql.tools.SchemaParser;
 import javax.servlet.annotation.WebServlet;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
 import graphql.schema.GraphQLSchema;
 import graphql.servlet.SimpleGraphQLServlet;
 
@@ -10,12 +11,18 @@ import graphql.servlet.SimpleGraphQLServlet;
 @WebServlet(urlPatterns = "/graphql")
 public class GraphQLEndpoint extends SimpleGraphQLServlet {
 
+    private static final LinkRepository linkRepository;
+
+    static {
+        MongoDatabase mongo = new MongoClient("ac-kdfo2zh-shard-00-02.ltisdlb.mongodb.net:27017").getDatabase("hackernews");
+        linkRepository = new LinkRepository(mongo.getCollection("links"));
+    }
+
     public GraphQLEndpoint() {
         super(buildSchema());
     }
 
     private static GraphQLSchema buildSchema() {
-        LinkRepository linkRepository = new LinkRepository();
         return SchemaParser.newParser()
                 .file("schema.graphqls")
                 .resolvers(new Query(linkRepository), new Mutation(linkRepository))
