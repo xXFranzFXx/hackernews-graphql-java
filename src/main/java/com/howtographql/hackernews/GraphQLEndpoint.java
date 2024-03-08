a66fc562533c4cc15844ac00c83034cc22da548f
xXFranzFXx <franz.fernando@testpro.io>
2024-03-07 17:20:13 -0800

    adds authContext

--- src/main/java/com/howtographql/hackernews/GraphQLEndpoint.java
+++ src/main/java/com/howtographql/hackernews/GraphQLEndpoint.java
@@ -1,22 +1,36 @@
 package com.howtographql.hackernews;
 import com.coxautodev.graphql.tools.SchemaParser;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoDatabase;
 import graphql.schema.GraphQLSchema;
+import graphql.servlet.GraphQLContext;
 import graphql.servlet.SimpleGraphQLServlet;
+
+import java.util.Optional;
 
 
 @WebServlet(urlPatterns = "/graphql")
 public class GraphQLEndpoint extends SimpleGraphQLServlet {
-
+    @Override
+    protected GraphQLContext createContext(Optional<HttpServletRequest> request, Optional<HttpServletResponse> response) {
+        User user = request
+                .map(req -> req.getHeader("Authorization"))
+                .filter(id -> !id.isEmpty())
+                .map(id -> id.replace("Bearer ", ""))
+                .map(userRepository::findById)
+                .orElse(null);
+        return new AuthContext(user, request, response);
+    }
     private static final LinkRepository linkRepository;
     private static final UserRepository userRepository;
 
 
     static {
-        MongoDatabase mongo = new MongoClient("mongodb+srv://omnichordplace:lPfF65WNR2bkBq8R@cluster0.ltisdlb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").getDatabase("hackernews");
+        MongoDatabase mongo = new MongoClient().getDatabase("hackernews");
         linkRepository = new LinkRepository(mongo.getCollection("links"));
         userRepository = new UserRepository(mongo.getCollection("users"));
 
@@ -29,7 +43,10 @@ public class GraphQLEndpoint extends SimpleGraphQLServlet {
     private static GraphQLSchema buildSchema() {
         return SchemaParser.newParser()
                 .file("schema.graphqls")
-                .resolvers(new Query(linkRepository), new Mutation(linkRepository, userRepository))
+                .resolvers(
+                        new Query(linkRepository),
+                        new Mutation(linkRepository, userRepository),
+                        new SigninResolver())
                 .build()
                 .makeExecutableSchema();
     }
--- src/main/java/com/howtographql/hackernews/Mutation.java
+++ src/main/java/com/howtographql/hackernews/Mutation.java
@@ -1,6 +1,7 @@
 package com.howtographql.hackernews;
 
 import com.coxautodev.graphql.tools.GraphQLRootResolver;
+import graphql.GraphQLException;
 
 public class Mutation implements GraphQLRootResolver {
 
@@ -12,13 +13,20 @@ public class Mutation implements GraphQLRootResolver {
         this.userRepository = userRepository;
     }
 
-    public Link createLink(String url, String description) {
-        Link newLink = new Link(url, description);
+    public Link createLink(String url, String description, String userId) {
+        Link newLink = new Link(url, description, userId);
         linkRepository.saveLink(newLink);
         return newLink;
     }
     public User createUser(String name, AuthData auth) {
         User newUser = new User(name, auth.getEmail(), auth.getPassword());
         return userRepository.saveUser(newUser);
+    }
+    public SigninPayload signinUser(AuthData auth) throws IllegalAccessException {
+        User user = userRepository.findByEmail(auth.getEmail());
+        if (user.getPassword().equals(auth.getPassword())) {
+            return new SigninPayload(user.getId(), user);
+        }
+        throw new GraphQLException("Invalid credentials");
     }
 }
