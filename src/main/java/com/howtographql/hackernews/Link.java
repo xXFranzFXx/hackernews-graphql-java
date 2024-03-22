52564138515dc7d3ce0743c2fd39d013d146a92a
xXFranzFXx <franz.fernando@testpro.io>
2024-03-22 08:37:24 -0700

    Update Link.java

--- src/main/java/com/howtographql/hackernews/Link.java
+++ src/main/java/com/howtographql/hackernews/Link.java
@@ -5,12 +5,11 @@ public class Link {
     private final String url;
     private final String description;
     private final String userId;
-
-
+    
     public Link(String url, String description, String userId) {
         this(null, url, description, userId);
     }
-    
+
     public Link(String id, String url, String description, String userId) {
         this.id = id;
         this.url = url;
