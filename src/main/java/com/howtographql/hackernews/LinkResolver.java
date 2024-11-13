10550c5f789438ef9bdfb76afa8d7065213cd00c
xXFranzFXx <franz.fernando@testpro.io>
2024-11-13 10:21:43 -0800

    Update LinkResolver.java

--- src/main/java/com/howtographql/hackernews/LinkResolver.java
+++ src/main/java/com/howtographql/hackernews/LinkResolver.java
@@ -3,7 +3,6 @@ package com.howtographql.hackernews;
 import com.coxautodev.graphql.tools.GraphQLResolver;
 
 public class LinkResolver implements GraphQLResolver<Link> {
-
     private final UserRepository userRepository;
 
     public LinkResolver(UserRepository userRepository) {
