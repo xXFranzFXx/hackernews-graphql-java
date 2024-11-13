5da85685c84288d7a94541951eba08afa8201fe6
xXFranzFXx <franz.fernando@testpro.io>
2024-11-13 10:23:11 -0800

    Update SigninResolver.java

--- src/main/java/com/howtographql/hackernews/SigninResolver.java
+++ src/main/java/com/howtographql/hackernews/SigninResolver.java
@@ -1,8 +1,8 @@
 package com.howtographql.hackernews;
 import com.coxautodev.graphql.tools.GraphQLResolver;
 
-public class SigninResolver implements GraphQLResolver<SigninPayload> {
-    public User user(SigninPayload payload) {
+public class SigninResolver implements GraphQLResolver<SignInPayload> {
+    public User user(SignInPayload payload) {
         return payload.getUser();
     }
 }
