1410bd9014035d66f7837040f02e0a7543d011ca
xXFranzFXx <franz.fernando@testpro.io>
2025-01-04 21:08:25 -0800

    Update SignInPayload.java

--- src/main/java/com/howtographql/hackernews/SignInPayload.java
+++ src/main/java/com/howtographql/hackernews/SignInPayload.java
@@ -3,7 +3,6 @@ package com.howtographql.hackernews;
 public class SignInPayload {
     private final String token;
     private final User user;
-
     public SignInPayload(String token, User user) {
         this.token = token;
         this.user = user;
