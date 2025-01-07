b7d28f0f16e00d24451649fb6bc85f630db94d29
xXFranzFXx <franz.fernando@testpro.io>
2025-01-06 18:07:06 -0800

    Update AuthContext.java

--- src/main/java/com/howtographql/hackernews/AuthContext.java
+++ src/main/java/com/howtographql/hackernews/AuthContext.java
@@ -7,14 +7,11 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.Optional;
 
 public class AuthContext extends GraphQLContext {
-
     private final User user;
-
     public AuthContext(User user, Optional<HttpServletRequest> request, Optional<HttpServletResponse> response) {
         super(request, response);
         this.user = user;
     }
-
     public User getUser() {
         return user;
     }
